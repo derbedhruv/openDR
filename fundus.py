d6bcd8d8ea70a942b4277cbc9bb66c9643c46580
OWL rPI <joshi@lvpei.org>
2016-05-11 09:02:39 +0000

    renamed LEDs to the colour of the wires.

--- fundus.py
+++ fundus.py
@@ -15,22 +15,21 @@ import time
 import picamera
 import RPi.GPIO as GPIO
 
-# set the pins
-white = 14
-# ^^^this pin is active LOW, hence note the True and False being counterintuitive
-IR = 15
+# set the pins - names are based on the colours of the wires connecting to the LEDs
+orangeyellow = 14	# NOTE: this pin is active LOW, hence FALSE is ON
+bluegreen  = 15
 switch = 18
-i=1
+i=1	# initial_counter
 
 # GPIO setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(switch, GPIO.IN, GPIO.PUD_UP)
-GPIO.setup(white,GPIO.OUT)
-GPIO.setup(IR, GPIO.OUT)
+GPIO.setup(orangeyellow ,GPIO.OUT)
+GPIO.setup(bluegreen, GPIO.OUT)
 
 # setting white ON but IR off
-GPIO.output(white, False)
-GPIO.output(IR, True)
+GPIO.output(orangeyellow, False)
+GPIO.output(bluegreen, False)
 
 with picamera.PiCamera() as camera:
     camera.resolution = (2592,1944);
@@ -38,22 +37,22 @@ with picamera.PiCamera() as camera:
         while True:
              filename = 'images/image',i,'.jpg'
              camera.start_preview()
-            # GPIO.output(IR, True)
              GPIO.wait_for_edge(switch,GPIO.RISING)
             
-	    # The following stuff happens when the buton is pressed
-             GPIO.output(IR, True)
-             GPIO.output(white, False)
+	    # Button is pressed
+             GPIO.output(bluegreen, True)
+             GPIO.output(orangeyellow, False)
              camera.capture('images/image'+str(i)+'.jpg',use_video_port=True)
+
 	    #Set the white LED OFF
-             GPIO.output(white, False)
-             GPIO.output(IR, True)
+             GPIO.output(orangeyellow, True)
+             GPIO.output(bluegreen, False)
              camera.stop_preview()
              i=i+1
     except  KeyboardInterrupt: 
     #    except ValueError:
     
         print "Interrupted"
-        GPIO.output(white, True)
-        GPIO.output(IR, True)
+        GPIO.output(orangeyellow, False)
+        GPIO.output(bluegreen, True)
         GPIO.cleanup()
