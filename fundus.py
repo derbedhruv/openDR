373c900a79f6a64694af397ba24f0ad7036f3650
OWL rPI <pi@raspberrypi.(none)>
2016-05-31 12:47:40 +0000

    included pigpio, removed camera_sensor_mode part which was giving an error on Wheezy.

--- fundus.py
+++ fundus.py
@@ -14,60 +14,63 @@
 
 import time
 import picamera
-import RPi.GPIO as GPIO
+import pigpio
 
 # set the pins - names are based on the colours of the wires connecting to the LEDs
-# NOTE: Both the orangeyellow and bluegreen LEDs are active LOW, hence False is ON and vice versa
-orangeyellow = 14
-bluegreen  = 15
-switch = 18
+# NOTE: Both the orangeyellow and bluegreen LEDs are active LOW, hence 0 is ON and vice versa
+orangeyellow = 2
+bluegreen  = 3
+switch = 4
 i=1	# initial_counter
 
-# GPIO setup
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(switch, GPIO.IN, GPIO.PUD_UP)
-GPIO.setup(orangeyellow ,GPIO.OUT)
-GPIO.setup(bluegreen, GPIO.OUT)
+# pi is initialized as the pigpio object
+pi=pigpio.pi()
 
+pi.set_mode(orangeyellow,pigpio.OUTPUT)
+pi.set_mode(bluegreen,pigpio.OUTPUT)
+pi.set_mode(switch,pigpio.INPUT)
+#set a pull-up resistor on the switch pin
+pi.set_pull_up_down(switch,pigpio.PUD_UP)
 # Defining functions for putting off each LED
 def normalON():
     # orangeyellow is ON and the other is OFF
-    GPIO.output(orangeyellow, False)
-    GPIO.output(bluegreen, True)
+    pi.write(orangeyellow,0)
+    pi.write(bluegreen,1)
 
 def secondaryON():
     # toggle
-    GPIO.output(orangeyellow, True)
-    GPIO.output(bluegreen, False)
+    pi.write(orangeyellow,1)
+    pi.write(bluegreen,0)
     
 # Begin the polling for the switch
 normalON()
-
-with picamera.PiCamera(sensor_mode=3) as camera:
+with picamera.PiCamera() as camera:
     camera.resolution =  camera.MAX_IMAGE_RESOLUTION
     try:
+        
         while True:
-             filename = 'images/image',i,'.jpg'
-             camera.start_preview()
-             GPIO.wait_for_edge(switch,GPIO.RISING)
-            
-	     # Button is pressed
+                filename = 'images/image',i,'.jpg'
+                camera.start_preview()
+                time.sleep(0.01)
+                pi.wait_for_edge(switch,pigpio.RISING_EDGE)
+             
+             # Button is pressed
              # First capture a picture with the first LED on
-			 # use_video_port=False enables capturing the image using the STILL port giving max resolution, instead of the video port. DO NOT CHANGE.
-             camera.capture('images/image' + str(i) + '_1.jpg', use_video_port=False)
+             # use_video_port=False enables capturing the image using the STILL port giving max resolution, instead of the video port. DO NOT CHANGE.
+                camera.capture('images/image' + str(i) + '_1.jpg', use_video_port=False)
 
              # Then capture with the second LED
-             secondaryON() 
-             camera.capture('images/image' + str(i) + '_2.jpg', use_video_port=False)
+                secondaryON() 
+                camera.capture('images/image' + str(i) + '_2.jpg', use_video_port=False)
 
-	     # Reset LED states
-             normalON()
-             camera.stop_preview()
-             i=i+1
+             # Reset LED states
+                normalON()
+                camera.stop_preview()
+                i=i+1
     except  KeyboardInterrupt: 
     #    except ValueError:
-    
-        print "Interrupted"
-        GPIO.output(orangeyellow, False)
-        GPIO.output(bluegreen, True)
-        GPIO.cleanup()
+        print 'Interrupted'
+        pi.write(orangeyellow,0)
+        pi.write(bluegreen,0)
+	camera.stop_preview()
+	pi.stop()
