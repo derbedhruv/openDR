62c02c035dfb3240f19718d2f7063c28dd28c613
Ebin Philip <stargazer18@gmail.com>
2016-06-16 18:07:04 +0530

    doesn't work

--- fundus_detect_autothresh.py
+++ fundus_detect_autothresh.py
@@ -15,7 +15,7 @@ cv2.imshow('image',img)
 
 circles = cv2.HoughCircles(cimg, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 200)
 
-if circles is None:
+if circles is not None:
     print "no circles found"
     center=(1500,968)
     radius=828
@@ -52,10 +52,35 @@ img_final[:,:,0]=b
 img_final[:,:,1]=g
 img_final[:,:,2]=r
 
+print "done with part1"
+
+mask=cv2.cvtColor(img_final,cv2.COLOR_BGR2GRAY)
+ret,mask=cv2.threshold(mask,100,255,cv2.THRESH_BINARY_INV)
+circles = cv2.HoughCircles(mask, cv2.cv.CV_HOUGH_GRADIENT, 1.2, 200)
+circles = np.uint16(np.around(circles))
+print 'done'
+for i in circles[0,:]:
+        # draw the outer circle
+##        image=img.copy()
+##        center=(i[0],i[1])
+##        radius=i[2]
+        cv2.circle(mask,(i[0],i[1]),i[2],(0,255,0),2)
+        # draw the center of the circle
+        cv2.circle(mask,(i[0],i[1]),2,(0,0,255),3)
+
+b=np.multiply(img_final[:,:,0],mask)
+g=np.multiply(img_final[:,:,1],mask)
+r=np.multiply(img_final[:,:,2],mask)
+img_final=np.zeros(img2.shape[:])
+img_final=np.uint8(img_final)
+img_final[:,:,0]=b
+img_final[:,:,1]=g
+img_final[:,:,2]=r
+
 mask=imutils.resize(mask,width=648)
 filtered=imutils.resize(img_final,width=648)
 # cv2.imshow('detected circles',img)
-#cv2.imshow('mask',mask)
+cv2.imshow('mask',mask)
 cv2.imshow('filtered',filtered)
 cv2.imwrite(loc+'processed/'+name,img_final)
 cv2.waitKey(0)
