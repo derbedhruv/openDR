62961c682595bc3fe5b6a33b30f61f551c1e8c1f
ayush7 <ayushyadav173@gmail.com>
2016-06-16 10:37:49 +0530

    Merged Ebin's code of cv2 fundus-mod2 with flask

--- fundus.py
+++ fundus.py
@@ -27,12 +27,16 @@ from flask import redirect, session, g, url_for, flash
 from flask import Response
 from Fundus_Cam import Fundus_Cam
 
+###################### Taken from fundus-mod2-- Remove comment if it works
+import io
+import cv2
+from picamera.array import PiRGBArray
+#######################
+
 
 
 #-------------------Flask implementation starts here--------------------#
 
-
-###processed_text = ''
 #create flask app
 app = Flask(__name__)
 
@@ -46,7 +50,7 @@ def my_form_post():
     global processed_text
     text = request.form['text']
     processed_text = text.upper()
-    make_a_dir(processed_text)
+    make_a_dir(processed_text)#sends data for foldername to make_a_dir()
     return redirect(url_for('captureSimpleFunc'))
     #fundusRun(processed_text)
     
@@ -72,7 +76,7 @@ def captureSimpleFunc():
         return redirect(url_for('captureSimpleFunc'))
 
 
-#Cam flask routes -----------------------#-------------------------------
+#--Cam flask routes --------------------#-------------------------------
 
 @app.route('/video_feed')
 def video_feed():
@@ -92,20 +96,8 @@ if __name__ == '__main__':
     app.run()
 
 
-
-
-
-
-
 #..........Below this line, all the functions not having flask lie.........#
-
-
-
-
-
-
     
-
 
 
 #make a directory of patient's name if it does not exist
@@ -117,46 +109,68 @@ def make_a_dir(pr_t):
 
 
 # set the pins - names are based on the colours of the wires connecting to the LEDs
+#The below comment has to be corrected by Ebin
 # NOTE: Both the orangeyellow and bluegreen LEDs are active LOW, hence 0 is ON and vice versa
-orangeyellow = 2
-bluegreen  = 3
-switch = 4
+camera=picamera.PiCamera() 
+
+led_top=2
+led_bottom=3
+capture_switch=4
 i=1	# initial_counter
 
 # pi is initialized as the pigpio object
 pi=pigpio.pi()
 
-pi.set_mode(orangeyellow,pigpio.OUTPUT)
-pi.set_mode(bluegreen,pigpio.OUTPUT)
-pi.set_mode(switch,pigpio.INPUT)
+#set I/O modes for GPIOs
+pi.set_mode(led_top,pigpio.OUTPUT)
+pi.set_mode(led_bottom,pigpio.OUTPUT)
+pi.set_mode(capture_switch,pigpio.INPUT)
+
 #set a pull-up resistor on the switch pin
-pi.set_pull_up_down(switch,pigpio.PUD_UP)
+pi.set_pull_up_down(capture_switch,pigpio.PUD_UP)
+
 # Defining functions for putting off each LED
 def normalON():
     # orangeyellow is ON and the other is OFF
-    pi.write(orangeyellow,0)
-    pi.write(bluegreen,1)
-    
+    pi.write(led_top,0)
+    pi.write(led_bottom,1)
 
 def secondaryON():
     # toggle
-    pi.write(orangeyellow,1)
-    pi.write(bluegreen,0)
+    pi.write(led_top,1)
+    pi.write(led_bottom,0)
+
+
+###--Next function commented because it's elements have been included in fundusRun() fuction
+# #function for capturing HQ images
+# def take_a_pic(pr_tx):  
+#     #An external file named 'name' stores a key for the number of times the capture button has been clicked
+#     #This helps in naming the files as the value is read and appended to image name
+#     #This also removes any sort of problem which can ever be caused in naming 
+#     #This method is foolproof
+
+
+#     #read file  
+#     filer = open('name','r')
+#     picn = (int)(filer.read())
+#     picn = picn+1
+#     filer.close()
+
+#     #increment the iteration key in file
+#     filew = open('name','w')
+#     filew.write(str(picn))
+#     filew.close()
+
+#     camera.capture('images/'+pr_tx+'/'+ str(picn) + '_' + str(i) + '_1.jpg', use_video_port=False)
+
+#  # Then capture with the second LED
+#     secondaryON() 
+#     camera.capture('images/'+pr_tx+'/' + str(picn) + '_' + str(i) + '_2.jpg', use_video_port=False)
     
-
-
-#function for capturing HQ images
-def take_a_pic(pr_tx):    
-    camera.capture('images/'+pr_tx+'/' + str(i) + '_1.jpg', use_video_port=False)
-
- # Then capture with the second LED
-    secondaryON() 
-    camera.capture('images/'+pr_tx+'/' + str(i) + '_2.jpg', use_video_port=False)
-    
- # Reset LED states
-    normalON()
-    camera.stop_preview()
-    i=i+1
+#  # Reset LED states
+#     normalON()
+#     camera.stop_preview()
+#     i=i+1
 
 
 
@@ -165,7 +179,7 @@ def take_a_pic(pr_tx):
 # Begin the polling for the switch
 def fundusRun(pr_t):
     normalON()
-    with picamera.PiCamera() as camera:
+    #with picamera.PiCamera() as camera:
         camera.resolution =  camera.MAX_IMAGE_RESOLUTION
         try:
         
@@ -174,27 +188,64 @@ def fundusRun(pr_t):
                     #camera.start_preview()
                     camera.vflip = False
                     time.sleep(0.01)
+                    stream=io.BytesIO()
+                    camera.start_preview()
                     pi.wait_for_edge(switch,pigpio.RISING_EDGE)
-                    take_a_pic(pr_t)
-                 # Button is pressed
-                 # First capture a picture with the first LED on
-                 # use_video_port=False enables capturing the image using the STILL port giving max resolution, instead of the video port. DO NOT CHANGE.
-                    
-                    
-                 #    camera.capture('images/'+pr_t+'/' + str(i) + '_1.jpg', use_video_port=False)
 
-                 # # Then capture with the second LED
-                 #    secondaryON() 
-                 #    camera.capture('images/'+pr_t+'/' + str(i) + '_2.jpg', use_video_port=False)
-    
-                 # # Reset LED states
-                 #    normalON()
-                 #    camera.stop_preview()
-                 #    i=i+1
+
+                    t1=cv2.getTickCount()
+                    camera.capture(stream,format='jpeg',use_video_port=True)
+
+
+                    image1=np.fromstring(stream.getvalue(),dtype=np.uint8)
+                    stream.truncate()
+                    #secondaryON()
+                    camera.capture(stream,format='jpeg',use_video_port=True)
+                    
+                    image2=np.fromstring(stream.getvalue(),dtype=np.uint8)
+                    t2=cv2.getTickCount()
+                    stream.truncate()
+                    time_taken=(t2-t1)/cv2.getTickFrequency()
+                    print "files captured time taken:"+str(time_taken)
+                    image1=cv2.imdecode(image1,1)
+                    image2=cv2.imdecode(image2,1)
+                    camera.stop_preview()
+                    #name=raw_input("enter the name to be saved")
+
+                    #An external file named 'name' stores a key for the number of times the capture button has been clicked
+                    #This helps in naming the files as the value is read and appended to image name
+                    #This also removes any sort of problem which can ever be caused in naming 
+                    #This method is foolproof
+
+                    #read file  
+                    filer = open('name','r')
+                    picn = (int)(filer.read())
+                    picn = picn+1
+                    filer.close()
+
+                    #increment the iteration key in file
+                    filew = open('name','w')
+                    filew.write(str(picn))
+                    filew.close()
+
+                    cv2.imwrite('images/'+pr_tx+'/'+ str(picn) + '_' + str(i) + '_1.jpg',image1)
+                    cv2.imwrite('images/'+pr_tx+'/'+ str(picn) + '_' + str(i) + '_2.jpg',image2)
+                    t3=cv2.getTickCount()
+                    time_taken=(t3-t1)/cv2.getTickFrequency()
+                    print "files saved time taken:"+str(time_taken)
+
+                    #camera.capture('images/'+pr_tx+'/'+ str(picn) + '_' + str(i) + '_1.jpg', use_video_port=False)
+
+                    # Then capture with the second LED
+                    #secondaryON() 
+                    #camera.capture('images/'+pr_tx+'/' + str(picn) + '_' + str(i) + '_2.jpg', use_video_port=False)
+                    
+                    # Reset LED states
+                    normalON()
+                    camera.stop_preview()
+                    i=i+1
         except  KeyboardInterrupt: 
         #   except ValueError:
             print 'Interrupted'
-            pi.write(orangeyellow,0)
-            pi.write(bluegreen,0)
             camera.stop_preview()
             pi.stop()
