9e52218319c35871f7a7be967003a3392d179d35
Ebin Philip <stargazer18@gmail.com>
2016-06-18 17:44:41 +0530

    capture,capture_continuous, flip, preview
    The following functions :
    capture()
    continuous_capture()
    preview()
    stop_preview()
    cam_flip()
    decode_image()
    ,

--- Fundus_Cam.py
+++ Fundus_Cam.py
@@ -1,3 +1,10 @@
+                        #############################
+                        ## Fundus_Cam.py           ##
+                        ## Primary Author: Ebin    ##
+                        ## Version : 1.0           ##
+                        #############################
+
+
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 from threading import Thread
@@ -5,17 +12,33 @@ import cv2
 import numpy as np
 import io
 
+#########################################################################
+# This class provide access to the picamera and its associated functions#
+#########################################################################
+
 class Fundus_Cam(object):
+
+    # The constructor initializes the camera object and starts preview
     def __init__(self, framerate=12,preview=False):
-        # initialize the camera and stream
+
+        # initialize the camera 
         self.camera = PiCamera()
         self.camera.resolution = self.camera.MAX_IMAGE_RESOLUTION
         self.camera.framerate = framerate
-        self.stream = io.BytesIO()
-        self.flip_state=False
 
+        # stream is a file-like type used to store captured images to memory
+        # rather than onto disk
+        self.stream = io.BytesIO()
+
+        # this determines the vertical flip_state of the picamera
+        # this can be toggled by Fundus_Cam.flip_cam()
+        self.flip_state=False
+        
+        # This is a list to store images captured in Fundus_Cam.continuous_capture()
         self.images=[]
         self.camera.start_preview()
+
+        # used to stop and start recording in versions higher than1.0
         self.stopped = False
 
     def continuous_capture(self):
@@ -24,41 +47,58 @@ class Fundus_Cam(object):
         Thread(target=self.update, args=()).start()
         
 
-    # continuosly grabs frames from the camera, in a seperate thread
+    # continuosly captures frames from the camera, in a seperate thread
     def update(self):
         # keep looping infinitely until the thread is stopped
+        # In version 1.0 it grabs only 10 frames
         while True:
-                # grab the frame from the stream and clear the stream in
-                # preparation for the next frame
+                # grab the frame from the stream 
                 self.camera.capture(self.stream,format='jpeg',use_video_port=True)
+                # convert the frame captures to numpy array and append to images
                 self.images.append(np.fromstring(self.stream.getvalue(),dtype=np.uint8))
+                # clear the stream for the next frame
                 self.stream.truncate()
                 self.stream.seek(0)
                 if(len(self.images)>9):
                     self.stopped=True
                     return
-##                
+                
     
-
+    # to flip the camera 
     def flip_cam():
         self.camera.vflip=(not self.flip_state)
 
-    #to capture and store images 
+    #to capture a single image
     def capture(self):
         self.camera.capture(self.stream,format='jpeg',use_video_port=True)
+        # convert to numpy array
         self.image=np.fromstring(self.stream.getvalue(),dtype=np.uint8)
+        # clear the stream
         self.stream.truncate()
         self.stream.seek(0)
+        # return the captured image
         return self.image
 
+    # to start camera preview
     def preview(self):
         self.camera.start_preview()
+
+    # to stop camera preview
     def stop_preview(self):
         self.camera.stop_preview()
-    
+
+    # used in version higher than 1.0
     def stop(self):
         self.stopped=True
+        
+   
+######        End of Class Implementation  ############
+#######################################################
 
+
+        
+
+# decode,process and save the grabbed image
 def decode_image(images):
     name=raw_input("enter the name to be saved")
     if type(images) is list:
@@ -75,7 +115,7 @@ def decode_image(images):
             
         
 
-
+# for debugging
 if __name__=='__main__':
     
     fundus_cam=Fundus_Cam()
