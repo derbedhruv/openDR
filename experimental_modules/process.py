0acd68205865bedde179e9b2e7ab6fd8274d1750
Dhruv Joshi <dhruv.joshi.1989@gmail.com>
2016-08-08 14:54:33 +0000

    TODO list for process.py

--- experimental_modules/process.py
+++ experimental_modules/process.py
@@ -4,7 +4,13 @@ import fundus
 import remove_glare
 import theia
 
-# This should go as an argument
+## TODO:
+## ------------
+## 1. Image parameters (center x,y, radius) should be arguments
+## 2. Can integrate all functions into single module
+## 3. Image file should go as an argument and processed image returned, i.e.
+##    this file should also be converted to a module
+
 # process.py should be called on every image taken
 filename = 'test.jpg'
 output = "out.jpg"
